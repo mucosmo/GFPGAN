3e6e8916e7c134166a15adc3cf024da5265fc730
huanyu.yang <mfuture@qq.com>
2023-08-30 11:44:43 +0800

    archive on Aug 30

--- inference_gfpgan.py
+++ inference_gfpgan.py
@@ -64,6 +64,7 @@ def main():
                           'If you really want to use it, please modify the corresponding codes.')
             bg_upsampler = None
         else:
+            print('-----here i am ---')
             from basicsr.archs.rrdbnet_arch import RRDBNet
             from realesrgan import RealESRGANer
             model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
